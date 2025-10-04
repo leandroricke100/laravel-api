776712cb20f55c57b1a26e7c2b510347b7f51b98
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2025-10-04 08:43:46 -0600

    banco de comentarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory(10)->create();
+        Post::factory(10)->has(Comment::factory()->count(3))->create();
         // User::factory(10)->create();
 
         // User::factory()->create([
